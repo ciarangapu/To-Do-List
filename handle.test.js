e740eaae021954fd8cb25cc08c3202c4b2d70fd5
ciarangapu <ceriangapu@gmail.com>
2025-02-17 21:43:16 +0100

    add all files

---
+++ handle.test.js
@@ -0,0 +1,18 @@
+const handleDropAndDrag  = {
+    delicious: true,
+    sour: false,
+  };
+  
+  describe('handle drop', () => {
+    test('if drag and drop  delicious ', () => {
+      expect(handleDropAndDrag.delicious).toBeTruthy();
+    });
+  
+    test('if does not drag and drop sour', () => {
+      expect(handleDropAndDrag.sour).toBeFalsy();
+    });
+  });
+
+
+
+  
