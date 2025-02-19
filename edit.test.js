c9348f0c2665a1a32784e12ff64c2398a4b5d7b5
ciarangapu <ceriangapu@gmail.com>
2025-02-19 15:50:02 +0100

    add all files

--- edit.test.js
+++ edit.test.js
@@ -1,14 +1,14 @@
-const Edit  = {
-    delicious: true,
-    sour: false,
-  };
-  
-  describe('handle drop', () => {
-    test(' if click on edit button, content should be editable and update ', () => {
-      expect(Edit.delicious).toBeTruthy();
-    });
-  
-    test('not editable if not click on edit button and content should not update', () => {
-      expect(Edit.sour).toBeFalsy();
-    });
+const Edit = {
+  delicious: true,
+  sour: false,
+};
+
+describe('handle drop', () => {
+  test(' if click on edit button, content should be editable and update ', () => {
+    expect(Edit.delicious).toBeTruthy();
   });
+
+  test('not editable if not click on edit button and content should not update', () => {
+    expect(Edit.sour).toBeFalsy();
+  });
+});
